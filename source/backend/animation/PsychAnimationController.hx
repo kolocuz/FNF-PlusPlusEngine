c557b1b8b04c15cfb81f888c7f6c8fd107af60d8
kolocuz <koloc232@gmail.com>
2026-07-31 19:19:26 +0300

    Update PsychAnimationController.hx

--- source/backend/animation/PsychAnimationController.hx
+++ source/backend/animation/PsychAnimationController.hx
@@ -5,14 +5,20 @@ import flixel.animation.FlxAnimationController;
 class PsychAnimationController extends FlxAnimationController {
     public var followGlobalSpeed:Bool = true;
 
-    public override function update(elapsed:Float):Void {
-		if (_curAnim != null) {
+    public override function update(elapsed:Float):Void
+    {
+        super.update(0);
+
+        if (_curAnim != null)
+        {
             var speed:Float = timeScale;
-            if (followGlobalSpeed) speed *= FlxG.animationTimeScale;
-			_curAnim.update(elapsed * speed);
-		}
-		else if (_prerotated != null) {
-			_prerotated.angle = _sprite.angle;
-		}
-	}
+            if (followGlobalSpeed)
+                speed *= FlxG.animationTimeScale;
+            _curAnim.update(elapsed * speed);
+        }
+        else if (_prerotated != null)
+        {
+            _prerotated.angle = _sprite.angle;
+        }
+    }
 }
